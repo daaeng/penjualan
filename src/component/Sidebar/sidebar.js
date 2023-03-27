4cb3e44ed2c0ec362e1a5375cb28b6c28c7cdb6a
daeng <daaeng.w@gmail.com>
2023-03-27 13:24:57 +0700

    ganti warna sdbar

--- src/component/Sidebar/sidebar.js
+++ src/component/Sidebar/sidebar.js
@@ -1,8 +1,9 @@
 import React, {useState } from 'react'
 import {MdDashboard, MdHome, MdShoppingCartCheckout, MdPayments, MdShoppingBasket } from 'react-icons/md'
 import {BsFillPersonVcardFill, BsArrowLeftCircleFill} from 'react-icons/bs'
-import {GiChessBishop} from 'react-icons/gi'
 import {NavLink, useNavigate} from 'react-router-dom'
+import {SiBathasu} from 'react-icons/si'
+// import {GiChessBishop} from 'react-icons/gi'
 // import {FaBars} from 'react-icons/fa'
 // import Logo from '../../assets/logo.png'
 //import { BiIdCard } from 'react-icons/bi'
@@ -27,19 +28,20 @@ function Sidebar() {
 
   return (
     <>                                                                  
-        <nav className = {` bg-red-700 h-screen py-2 pt-10 duration-300 ${open ? 'w-48' : 'w-20'} `}>
+        <nav className = {` bg-black h-screen py-2 pt-10 duration-300 ${open ? 'w-48' : 'w-20'} `}>
             
             <div className='py-2 px-3 overflow-hidden '>
                 
             <BsArrowLeftCircleFill 
-                        className={`text-red-700 bg-white text-2xl rounded-full 
+                        className={`text-black bg-white text-2xl rounded-full 
                         absolute -right-3 top-5 border border-dark-purple cursor-pointer ${!open && 'rotate-180'}` } 
                         onClick={()=> setOpen(!open)}/>
                     {/* onClick={()=> setOpen(!open)}     */}
 
                 <div className='inline-flex ml-1.5 mb-16'>
                             
-                    <GiChessBishop className={`bg-white rounded-md text-4xl cursor-pointer block float-left mr-2 duration-500 ${!open && 'rotate-[360deg]'}`} onClick={()=> navigate('/')}/>
+                    <SiBathasu className={`text-red-800 bg-black rounded-full text-4xl cursor-pointer 
+                                block float-left mr-2 duration-500 hover:text-yellow-600 ${!open && 'rotate-[360deg]'}`} onClick={()=> navigate('/')}/>
                     <h3 className={`text-white origin-left font-medium text-2xl duration-300 ${!open && 'scale-0'}`}>
                             Shoping
                     </h3>
@@ -55,7 +57,7 @@ function Sidebar() {
                                 
                                 className={`cursor-pointer no-underline flex flex-row duration-300 
                                 ${!open && 'active' }items-center text-gray-100 overflow-y-visible
-                                hover:bg-light-white hover:text-black rounded-xl px-2.5 py-4 -ml-1 -mr-7`} >
+                                hover:bg-light-white hover:text-yellow-600 rounded-xl px-2.5 py-4 -ml-1 -mr-7`} >
                                 
                                         
                                         <div className={`ml-2 text-2xl `}>{val.icon}</div>
