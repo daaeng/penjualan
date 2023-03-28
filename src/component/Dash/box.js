44384fd24bad61472a62f29f714d76243c0f96cb
daeng <daaeng.w@gmail.com>
2023-03-28 08:17:16 +0700

    Revert "erroran"
    This reverts commit b5cd7f3de341023c2467f769644dcdfa1f2b26ca, reversing
    changes made to 418e5e980c8e7435bc504135df37c39939f8ca97.

--- src/component/Dash/box.js
+++ src/component/Dash/box.js
@@ -11,22 +11,14 @@ function Box () {
     <>
       <div className='grid lg:grid-cols-8 gap-4 p-4 '>
             
-<<<<<<< HEAD
           <div className='shadow cursor-pointer hover:bg-green-300 lg:col-span-2 col-span-1 bg-green-100 flex justify-between w-full  p-4 rounded-lg'>
-=======
-          <div className='cursor-pointer hover:bg-green-300 lg:col-span-2 col-span-1 bg-green-100 flex justify-between w-full  p-4 rounded-lg'>
->>>>>>> 7c244e564ca1e5d6052bfab11f0b4ac401e5f066
               <FaUsers size={25}/>
               <span className='text-sm'>
                   Pelanggan
               </span>
           </div>
             
-<<<<<<< HEAD
           <div className='shadow cursor-pointer hover:bg-indigo-300 lg:col-span-2 col-span-1 bg-indigo-100 flex justify-between w-full  p-4 rounded-lg'>
-=======
-          <div className='cursor-pointer hover:bg-indigo-300 lg:col-span-2 col-span-1 bg-indigo-100 flex justify-between w-full  p-4 rounded-lg'>
->>>>>>> 7c244e564ca1e5d6052bfab11f0b4ac401e5f066
               <MdShoppingBasket size={25}/>
               <span className='text-sm'>
                   Item
@@ -35,11 +27,7 @@ function Box () {
           
           <div 
             onClick = {() => navigate ('/dpen')} 
-<<<<<<< HEAD
             className={`shadow cursor-pointer hover:bg-cyan-300 lg:col-span-2 
-=======
-            className={`cursor-pointer hover:bg-cyan-300 lg:col-span-2 
->>>>>>> 7c244e564ca1e5d6052bfab11f0b4ac401e5f066
             col-span-1 bg-cyan-100 flex justify-between w-full  
             p-4 rounded-lg `}>
                 <MdShoppingCartCheckout size={25}/>
@@ -49,11 +37,7 @@ function Box () {
                 </span>
           </div>
             
-<<<<<<< HEAD
           <div className='shadow cursor-pointer hover:bg-red-300 lg:col-span-2 col-span-1 bg-red-100 flex justify-between w-full  p-4 rounded-lg'>
-=======
-          <div className='cursor-pointer hover:bg-red-300 lg:col-span-2 col-span-1 bg-red-100 flex justify-between w-full  p-4 rounded-lg'>
->>>>>>> 7c244e564ca1e5d6052bfab11f0b4ac401e5f066
               <MdPayments size={25}/>
               <span className='text-sm'>
                   Pembayaran
--- src/component/Dash/search.js
+++ src/component/Dash/search.js
@@ -4,17 +4,11 @@ import { BiSearch } from 'react-icons/bi';
 function Search() {
   return (
     <div className='border border-gray-200 rounded-full w-full flex py-1 px-1 mt-1 items-center'>
-<<<<<<< HEAD
 
         <input type={'text'} className = 'rounded flex-1 ml-1 mr-16 focus:outline-none focus:ring focus:border-blue-500' placeholder = ' Search'/>
         <button type='submit' className='md:-ml-14 mr-1 bg-gray-200 px-1 py-1 rounded-md hover:bg-green-100'>
           <BiSearch/>
         </button>
-=======
-        <BiSearch/>
-        <input type={'text'} className = 'rounded flex-1 ml-1 mr-16 focus:outline-none focus:ring focus:border-blue-500' placeholder = ' Search'/>
-        <button type='submit' className='md:-ml-14 mr-1 bg-gray-200 px-1 rounded-md hover:bg-green-100'>Search</button>
->>>>>>> 7c244e564ca1e5d6052bfab11f0b4ac401e5f066
     </div>
   )
 }
