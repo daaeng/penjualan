6a5d7b98e707b977808c85f0a23bc6b2dadd6404
daeng <daaeng.w@gmail.com>
2023-07-15 16:45:55 +0700

    API testing

--- src/component/config/api/axios.js
+++ src/component/config/api/axios.js
@@ -5,8 +5,9 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "http://apisrc25.lan",
   // baseURL : 'https://dummy.restapiexample.com/api/v1/employees'
+  baseURL : 'https://dummyjson.com/'
   // baseURL : 'https://dummyjson.com/products'
-  baseURL: "https://192.168.21.70:13541"
+  // baseURL: "https://192.168.21.70:13541"
 });
 
 
